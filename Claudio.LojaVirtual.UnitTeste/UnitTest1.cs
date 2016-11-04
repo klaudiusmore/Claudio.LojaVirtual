0a6fe619ab5c336b6ce6491a849a1477e67b2002
Gisele <Gisele@PC-MONI>
2016-11-04 12:14:15 -0200

    Commit de Projeto

---
+++ Claudio.LojaVirtual.UnitTeste/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Claudio.LojaVirtual.UnitTeste
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
---
+++ Claudio.Lojavirtual.Web/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Claudio.Lojavirtual.Web.MvcApplication" Language="C#" %>
